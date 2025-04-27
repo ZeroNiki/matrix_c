431dd8a5ec0ad44e3967e15f57a519e77684c232
Nella Rory <nellaror@ji-f4.tash.21-school.uz>
2025-04-27 19:36:50 +0500

    Add more test cases

--- tests/test_remove_matrix.c
+++ tests/test_remove_matrix.c
@@ -9,11 +9,27 @@ START_TEST(test_remove_matrix) {
   ck_assert_ptr_null(mat.matrix);
 }
 
+START_TEST(test_remove_null_matrix) {
+  matrix_t *mat = NULL;
+  s21_remove_matrix(mat);
+}
+END_TEST
+
+START_TEST(test_remove_null_data_matrix) {
+  matrix_t mat;
+  mat.matrix = NULL;
+  mat.rows = 5;
+  s21_remove_matrix(&mat);
+}
+END_TEST
+
 Suite *create_s21_remove_matrix(void) {
   Suite *suite = suite_create("s21_matrix_remove");
 
   TCase *tc_core = tcase_create("Core");
   tcase_add_test(tc_core, test_remove_matrix);
+  tcase_add_test(tc_core, test_remove_null_matrix);
+  tcase_add_test(tc_core, test_remove_null_data_matrix);
   suite_add_tcase(suite, tc_core);
 
   return suite;
