ade981932b20d6d118b3fd0d3212cf24ea3510f5
Nella Rory <nellaror@ji-f4.tash.21-school.uz>
2025-04-27 18:51:08 +0500

    New funcion

---
+++ code/s21_eq_matrix.c
@@ -0,0 +1,21 @@
+#include "../s21_matrix.h"
+
+int s21_eq_matrix(matrix_t *A, matrix_t *B) {
+  if (A == NULL || B == NULL || A->matrix == NULL || B->matrix == NULL) {
+    return FAILURE;
+  }
+
+  if (A->rows != B->rows || A->cols != B->cols) {
+    return FAILURE;
+  }
+
+  for (int i = 0; i < A->rows; i++) {
+    for (int j = 0; j < A->cols; j++) {
+      if (fabs(A->matrix[i][j] - B->matrix[i][j]) > EPS) {
+        return FAILURE;
+      }
+    }
+  }
+
+  return SUCCESS;
+}
