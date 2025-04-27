be80013ad5fe00d816366a3f3cb35ef3462fe466
Nella Rory <nellaror@ji-f4.tash.21-school.uz>
2025-04-27 18:51:00 +0500

    Add new funcion eq_matrix and new error codes

--- s21_matrix.h
+++ s21_matrix.h
@@ -1,11 +1,20 @@
 #ifndef S21_MATRIX_H
 #define S21_MATRIX_H
 
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <math.h>
 
-#define SUCCESS 0
-#define FAILURE 1
-#define ERROR 2
+// Maximum permissible
+#define EPS 1e-7
+
+// Result code
+#define ZERO 0
+#define ONE 1
+#define TWO 2
+
+// Compare result code
+#define SUCCESS 1
+#define FAILURE 0
 
 typedef struct {
   double **matrix;
@@ -15,5 +24,7 @@ typedef struct {
 
 int s21_create_matrix(int rows, int columns, matrix_t *result);
 void s21_remove_matrix(matrix_t *A);
+
+int s21_eq_matrix(matrix_t *A, matrix_t *B);
 
 #endif
