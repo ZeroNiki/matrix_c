6f4d4c803c49a2a7e5a24d700193dd59e6ba9e2c
Nella Rory <nellaror@ji-f4.tash.21-school.uz>
2025-05-04 12:46:56 +0500

    Add more tests for eq

--- tests/test_eq_matrix.c
+++ tests/test_eq_matrix.c
@@ -1,18 +1,16 @@
 #include "../matrix_test.h"
 
-// TODO: Valgrind error fix
-
-START_TEST(test_eq_matrix_same) {
+START_TEST(test_eq_same_matrix) {
   matrix_t A, B;
   s21_create_matrix(2, 2, &A);
   s21_create_matrix(2, 2, &B);
 
-  A.matrix[0][0] = 1.0;
+  A.matrix[0][0] = 1.1234567;
   A.matrix[0][1] = 2.0;
   A.matrix[1][0] = 3.0;
   A.matrix[1][1] = 4.0;
 
-  B.matrix[0][0] = 1.0;
+  B.matrix[0][0] = 1.1234567;
   B.matrix[0][1] = 2.0;
   B.matrix[1][0] = 3.0;
   B.matrix[1][1] = 4.0;
@@ -24,24 +22,44 @@ START_TEST(test_eq_matrix_same) {
 }
 END_TEST
 
-START_TEST(test_null) { ck_assert_int_eq(s21_eq_matrix(NULL, NULL), 0); }
-END_TEST
+START_TEST(test_eq_diff_matrix) {
+  matrix_t A, B;
+  s21_create_matrix(2, 2, &A);
+  s21_create_matrix(2, 2, &B);
 
-START_TEST(test_null_A) {
-  matrix_t B;
-  s21_create_matrix(3, 3, &B);
+  A.matrix[0][0] = 1.0;
+  A.matrix[0][1] = 2.0;
+  A.matrix[1][0] = 3.0;
+  A.matrix[1][1] = 4.0;
 
-  B.matrix[0][0] = 1.0;
-  B.matrix[0][1] = 2.0;
-  B.matrix[1][0] = 3.0;
-  B.matrix[1][1] = 4.0;
+  B.matrix[0][0] = 5.0;
+  B.matrix[0][1] = 6.0;
+  B.matrix[1][0] = 7.0;
+  B.matrix[1][1] = 8.0;
 
-  ck_assert_int_eq(s21_eq_matrix(NULL, &B), 0);
+  ck_assert_int_eq(s21_eq_matrix(&A, &B), 0);
+
+  s21_remove_matrix(&A);
   s21_remove_matrix(&B);
 }
 END_TEST
 
-START_TEST(test_null_B) {
+START_TEST(test_eq_diff_size) {
+  matrix_t A, B;
+  s21_create_matrix(2, 2, &A);
+  s21_create_matrix(3, 2, &B);
+
+  ck_assert_int_eq(s21_eq_matrix(&A, &B), 0);
+
+  s21_remove_matrix(&A);
+  s21_remove_matrix(&B);
+}
+END_TEST
+
+START_TEST(test_null) { ck_assert_int_eq(s21_eq_matrix(NULL, NULL), 0); }
+END_TEST
+
+START_TEST(test_null_matrix) {
   matrix_t A;
   s21_create_matrix(3, 3, &A);
 
@@ -50,49 +68,23 @@ START_TEST(test_null_B) {
   A.matrix[1][0] = 3.0;
   A.matrix[1][1] = 4.0;
 
+  ck_assert_int_eq(s21_eq_matrix(NULL, &A), 0);
   ck_assert_int_eq(s21_eq_matrix(&A, NULL), 0);
   s21_remove_matrix(&A);
 }
 END_TEST
 
-START_TEST(test_null_matrix) {
+START_TEST(test_eq_null_internal_matrix) {
   matrix_t A, B;
   A.matrix = NULL;
-  B.matrix = NULL;
-
-  ck_assert_int_eq(s21_eq_matrix(&A, &B), 0);
-}
-END_TEST
-
-START_TEST(test_null_matrix_A) {
-  matrix_t A, B;
-  s21_create_matrix(3, 3, &B);
-
-  B.matrix[0][0] = 1.0;
-  B.matrix[0][1] = 2.0;
-  B.matrix[1][0] = 3.0;
-  B.matrix[1][1] = 4.0;
-
-  A.matrix = NULL;
-
-  ck_assert_int_eq(s21_eq_matrix(&A, &B), 0);
-  s21_remove_matrix(&B);
-}
-END_TEST
-
-START_TEST(test_null_matrix_B) {
-  matrix_t A, B;
-  s21_create_matrix(3, 3, &A);
-
-  A.matrix[0][0] = 1.0;
-  A.matrix[0][1] = 2.0;
-  A.matrix[1][0] = 3.0;
-  A.matrix[1][1] = 4.0;
+  A.rows = 2;
+  A.cols = 2;
 
   B.matrix = NULL;
+  B.rows = 2;
+  B.cols = 2;
 
   ck_assert_int_eq(s21_eq_matrix(&A, &B), 0);
-  s21_remove_matrix(&A);
 }
 END_TEST
 
@@ -100,13 +92,12 @@ Suite *create_s21_eq_matrix(void) {
   Suite *suite = suite_create("s21_eq_matrix");
 
   TCase *tc_core = tcase_create("Core");
-  tcase_add_test(tc_core, test_eq_matrix_same);
+  tcase_add_test(tc_core, test_eq_same_matrix);
+  tcase_add_test(tc_core, test_eq_diff_matrix);
+  tcase_add_test(tc_core, test_eq_diff_size);
   tcase_add_test(tc_core, test_null);
-  tcase_add_test(tc_core, test_null_A);
-  tcase_add_test(tc_core, test_null_B);
   tcase_add_test(tc_core, test_null_matrix);
-  tcase_add_test(tc_core, test_null_matrix_A);
-  tcase_add_test(tc_core, test_null_matrix_B);
+  tcase_add_test(tc_core, test_eq_null_internal_matrix);
   suite_add_tcase(suite, tc_core);
 
   return suite;
